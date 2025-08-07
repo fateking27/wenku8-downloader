bda5cac4c62d67f20b93255b98a4ef80e1810863
liangjinhai <fateking27@outlook.com>
2025-08-07 18:26:59 +0800

    refactor(start.bat): 改进启动脚本以自动检测并安装依赖
    添加依赖检查逻辑，当node_modules不存在时自动安装依赖
    更新README.md

--- start.bat
+++ start.bat
@@ -1,4 +1,20 @@
 @echo off
-:home
-node index.js
-goto home
+chcp 65001
+set "foldername=node_modules"
+if exist "%cd%\%foldername%\" (
+    node index.js
+    pause
+    goto home
+) else (
+    echo 缺少依赖，开始安装依赖...
+    echo 请确保已安装 Nodejs 和 Yarn。
+    echo 正在安装依赖...
+
+    yarn install
+    echo 依赖安装完成
+    echo 开始运行程序...
+    node index.js
+    pause
+    goto home
+)
+pause
